7b7f9090a358f1d44fe421ffc962ca5824b771aa
sheilaLygreWeitling <U2407194@rts-365.dk>
2021-08-24 08:25:22 +0200

    add solution to assignment

--- Opgaver/Niveau1/03/index.js
+++ Opgaver/Niveau1/03/index.js
@@ -2,7 +2,7 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     function hideElementViaCssSelector (cssSelector){
-        const hideCssElement  =document.querySelector(cssSelector);
+        const hideCssElement  = document.querySelector(cssSelector);
         hideCssElement.style.display = "none";
     };
 
