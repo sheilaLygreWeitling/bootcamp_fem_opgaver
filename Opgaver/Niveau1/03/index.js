cebd10647ef9ca946b2425261b06ca83d7562a07
sheilaLygreWeitling <U2407194@rts-365.dk>
2021-08-24 13:19:06 +0200

    add

--- Opgaver/Niveau1/03/index.js
+++ Opgaver/Niveau1/03/index.js
@@ -3,7 +3,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
     function hideElementViaCssSelector (cssSelector){
         const hideCssElement  = document.querySelector(cssSelector);
-        hideCssElement.style.display = "none";
+        if(hideCssElement){
+            hideCssElement.style.display = "none";
+        }
+        
     };
 
     hideElementViaCssSelector("#hideHeading")
